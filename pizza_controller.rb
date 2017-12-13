ebf722a902d9a80b40f83a96c44f9162871dc492
Joe Ryan <joeryansaccount@googlemail.com>
2017-12-13 16:03:07 +0000

    lab finished and css added

--- pizza_controller.rb
+++ pizza_controller.rb
@@ -26,3 +26,25 @@
     @pizza.save()
     erb( :create)
   end
+
+#Delete v
+
+  post('/pizzas/:id/delete') do
+    id = params['id']
+    @pizza = Pizza.find(id)
+    @pizza.delete
+    erb( :destroy)
+  end
+
+# Edit
+get('/pizzas/:id/edit') do
+  id = params['id']
+  @pizza = Pizza.find(id)
+  erb( :edit)
+end
+
+post('/pizzas/:id') do
+  @pizza = Pizza.new(params)
+  @pizza.update()
+  erb( :update)
+end
