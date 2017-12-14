02ea777a5437670ff68c5ddd681a90bc1a237d8a
Richard Allison <richard.allison.18@outlook.com>
2017-12-14 00:06:01 +0000

    Some changes to layout and styling

--- pizza_controller.rb
+++ pizza_controller.rb
@@ -1,50 +1,54 @@
-  require( 'sinatra' )
-  require( 'sinatra/contrib/all' )
-  require( 'pry-byebug' )
-  require_relative('models/pizza.rb')
+require( 'sinatra' )
+require( 'sinatra/contrib/all' ) if development?
+require( 'pry-byebug' )
+require_relative('./models/pizza.rb')
 
+# Index
+get('/pizzas') do
+  @pizzas = Pizza.all()
+  erb(:index)
+end
 
-  get('/pizzas') do
-    @pizzas = Pizza.all()
+# New
+get('/pizzas/new') do
+  erb(:new)
+end
 
-    erb( :index)
-  end
+# Show
+get('/pizzas/:id') do
+  id = params[:id]
+  @pizza = Pizza.find(id)
+  erb(:show)
+end
 
-  get('/pizzas/new') do
-    erb ( :new)
-  end
+# Create
+post('/pizzas') do
+  @pizza = Pizza.new(params)
+  @pizza.save()
+  erb(:create)
+end
 
-
-  get('/pizzas/:id') do
-    id = params['id']
-    @pizza = Pizza.find(id)
-    erb( :show)
-  end
-
-  post('/pizzas') do
-    @pizza = Pizza.new(params)
-    @pizza.save()
-    erb( :create)
-  end
-
-#Delete v
-
-  post('/pizzas/:id/delete') do
-    id = params['id']
-    @pizza = Pizza.find(id)
-    @pizza.delete
-    erb( :destroy)
-  end
+# Delete
+post('/pizzas/:id/delete') do
+  id = params['id']
+  @pizza = Pizza.find(id)
+  @pizza.delete
+  erb( :destroy)
+end
 
 # Edit
 get('/pizzas/:id/edit') do
-  id = params['id']
-  @pizza = Pizza.find(id)
-  erb( :edit)
+id = params['id']
+@pizza = Pizza.find(id)
+erb( :edit)
 end
 
+# Update
 post('/pizzas/:id') do
-  @pizza = Pizza.new(params)
-  @pizza.update()
-  erb( :update)
+@pizza = Pizza.new(params)
+@pizza.update()
+erb( :update)
 end
+
+# update preselect: "selected" - inserting a string into the html, which is a boolean attribute - either there or not, takes no value.
+# value = "Calzone" <%= "selected" if @pizza.topping == "Calzone" %> for each option in edit
